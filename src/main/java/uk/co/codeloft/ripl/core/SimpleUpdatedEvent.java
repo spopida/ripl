1e530500a5efc031609fd3223d0da22f6e3984e9
Adrian Hall <ade@adrian-hall.name>
2022-08-24 21:54:33 +0100

    New parameterised classes for simple updates

---
+++ src/main/java/uk/co/codeloft/ripl/core/SimpleUpdatedEvent.java
@@ -0,0 +1,17 @@
+package uk.co.codeloft.ripl.core;
+
+public class SimpleUpdatedEvent <T extends AggregateRoot, O extends Object> extends UpdatedEvent<T>{
+
+    public SimpleUpdatedEvent(SimpleUpdateCommand<T, O> command) {
+        super(command);
+    }
+
+    @Override
+    public T apply() {
+        T newVersion = this.updateAggregateRoot(); // TODO: change how this is done?
+
+        SimpleUpdateCommand<T, O> cmd = (SimpleUpdateCommand<T, O>) this.getCommand();
+        cmd.getFunc().accept(newVersion, cmd.getParam());
+        return cmd.getTarget();
+    }
+}
---
+++ src/main/java/uk/co/codeloft/ripl/core/UpdateCommandTemplate.java
@@ -0,0 +1,22 @@
+package uk.co.codeloft.ripl.core;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+
+public class UpdateCommandTemplate<T extends AggregateRoot, O> {
+    private final BiPredicate<T, O> preCondition;
+    private final BiConsumer<T, O> eventFunc;
+
+    public UpdateCommandTemplate(BiConsumer<T, O> eventFunc) {
+        this(null, eventFunc);
+    }
+
+    public UpdateCommandTemplate(BiPredicate<T, O> preCondition, BiConsumer<T, O> eventFunc) {
+        this.preCondition = preCondition;
+        this.eventFunc = eventFunc;
+    }
+
+    public SimpleUpdateCommand<T, O> using(T target, O o) {
+        return new SimpleUpdateCommand<>(target, this.preCondition, this.eventFunc, o);
+    }
+}
